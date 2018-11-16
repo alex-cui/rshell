c42bbaf5fda645a9f0d1a1dc644b642c277ced0a
Alex Cui <alexcui1@yahoo.com>
2018-11-15 20:15:16 -0800

    moved into src

---
+++ src/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include "gtest/gtest.h"
+
+using namespace std;
+
+TEST(ShellTest, lsTest) {
+  
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
