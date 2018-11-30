7874650a0162f92bd026d2c12eec3c8c1ee60092
Jacob Halvorson <jhalv001@ucr.edu>
2018-11-29 21:02:25 -0800

    fixed cmakelist list and added tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -5,7 +5,7 @@
 #include "../header/or.h"
 
 #include "../header/test.h"
-
+#include "../header/precedence.h"
 #include "../header/command.h"
 #include <iostream>
 #include <string>
@@ -15,20 +15,131 @@
 #include "gtest/gtest.h"
 
 int charIndex(char* temp, char c[]) {
-    int counter = -1;
-    
-    while (*temp != '\0') {
+    int counter = 0;
+
+    while (*temp != *c) {
         temp += 1;
         counter += 1;
     }
     return counter;
 }
 
+void parse(char* c, Precedence* p) {
+    char semicolon[] = ";";
+    char pound[] = "#";
+    string test = "test";
+    string ANDsym = "&&";
+    string ORsym = "||";
+    string openBrack = "[";
+    string closeBrack = "]";
+    string input = "";
+
+    Command* cmd = new Command();
+    Precedence* pre = new Precedence();
+
+    while (c[strlen(c) - 1] != ')') {
+        if (c[0] == '(') {
+            c += 1; 
+
+            p = new Precedence();
+            
+            
+            parse(c, pre);
+        }
+        else if (c == test) {
+            c = strtok(0, " ");
+
+            cmd = new Test();
+
+            if ((c[0] == '-') && (c[2] == '\0')) {
+                cmd->addFlag(c[1]);
+               c = strtok(0, " ");
+            }
+
+            cmd->addCmd(c);
+        }
+        if (strpbrk(c, "#") != NULL) {
+            if (c[0] == '#') {
+                return;
+            }
+            else {
+                c[charIndex(&c[0], pound)] = '\0';
+
+                cmd->addCmd(c);
+                return;
+            }
+        }
+        else if (c == openBrack) {
+            c = strtok(0, " ");
+
+            cmd = new Test();
+
+            if ((c[0] == '-') && (c[2] == '\0')) {
+                cmd->addFlag(c[1]);
+                c = strtok(0, " ");
+            }
+
+            cmd->addCmd(c);
+
+            while (c != closeBrack) {
+                c = strtok(0, " ");
+                cmd->addCmd(c);
+            }
+        }
+        else if (strpbrk(c, semicolon) != NULL) {
+            c[strlen(c) - 1] = '\0';
+
+            cmd->addCmd(c);
+            p->add(cmd);
+                        cmd = new Command();
+        }
+        else if (c == ANDsym) {
+            if (!p->isEmpty()) {
+                p->add(pre);
+                p->add(new And(p));
+
+                p = new Precedence();
+            }
+            else {
+                p->add(cmd); 
+                p->add(new And(cmd));
+
+                cmd = new Command();
+            }
+        }
+        else if (c == ORsym) {
+            if (!p->isEmpty()) {
+                p->add(pre);
+                p->add(new Or(p));
+
+                p = new Precedence();
+            }
+            else {
+                p->add(cmd); 
+                p->add(new Or(cmd));
+
+                cmd = new Command();
+            }
+        }
+        else {
+            cmd->addCmd(c);
+        }
+
+        c = strtok (0, " ");
+    }
+    
+    
+    c[strlen(c) - 1] = '\0';
+    cmd->addCmd(c);
+
+    p->add(cmd);
+}
+
 int mainFunc(string input) {
     vector <Base*> v;
 
-    Connector* conn = 0;
     Command* cmd = new Command();
+    Precedence* p = new Precedence();
 
     char semicolon[] = ";";
     char pound[] = "#";
@@ -37,8 +148,6 @@ int mainFunc(string input) {
     string ORsym = "||";
     string openBrack = "[";
     string closeBrack = "]";
-    //string openParen = "(";
-    //string closeParen = ")";
     char *c = 0;
     
     if (input != "") {
@@ -48,108 +157,112 @@ int mainFunc(string input) {
     strtok(c, " ");
     
     while (c != 0) {
-	if (c == test) {
-                c = strtok(0, " "); 
+            if (c[0] == '(') {
+                c += 1; 
+
+                p = new Precedence();
+
+                parse(c, p);
+            }
+            else if (c == test) {
+                c = strtok(0, " ");
 
                 cmd = new Test(); 
 
+                
                 if ((c[0] == '-') && (c[2] == '\0')) {
                     cmd->addFlag(c[1]);
-                    c = strtok(0, " "); 
+                    c = strtok(0, " ");
                 }
 
-                cmd->addCmd(c);
-        }
-        else if (c == openBrack) {
-                c = strtok(0, " "); 
+                cmd->addCmd(c); 
+            }
+            else if (strpbrk(c, pound) != NULL) {
+                if (c[0] == '#') {
+                    break;
+                } 
+                else {
+                    c[charIndex(&c[0], pound)] = '\0';
+
+                    cmd->addCmd(c); 
+                    break;
+                }
+            }
+            else if (c == openBrack) {
+                c = strtok(0, " ");
 
                 cmd = new Test();
 
                 if ((c[0] == '-') && (c[2] == '\0')) {
                     cmd->addFlag(c[1]);
-                    c = strtok(0, " "); 
+                    c = strtok(0, " ");
                 }
 
                 cmd->addCmd(c);
 
+                
                 while (c != closeBrack) {
-                    c = strtok(0, " "); 
+                    c = strtok(0, " ");
                     cmd->addCmd(c);
                 }
-        }
-        else if (strpbrk(c, pound) != NULL) {
-                if (c[0] == '#') {
-                    break;
-                }
-                else {
-                    c[charIndex(&c[0], pound)] = '\0';
+            }
+            else if (strpbrk(c, semicolon) != NULL) {
                         
-                    cmd->addCmd(c); 
-                    break;
-                }
-        }
-        else if (strpbrk(c, semicolon) != NULL) {
-                c[charIndex(&c[0], semicolon)] = '\0'; 
-                    
+                c[strlen(c) - 1] = '\0';
+
                 cmd->addCmd(c);
-                v.push_back(cmd);
+                v.push_back(cmd); 
 
-                conn = new Semicolon(cmd);
-                v.push_back(conn);
-                
-                cmd = new Command();
-        }
-        else if (c == ANDsym) {
-                v.push_back(cmd);
-
-                conn = new And(cmd);
-                v.push_back(conn);
-                
-                cmd = new Command();
-        }
-        else if (c == ORsym) {
-                v.push_back(cmd);
-
-                conn = new Or(cmd);
-                v.push_back(conn);
-                
-                cmd = new Command();
-        }
-        else {
-                cmd->addCmd(c);
-        }
-      
-        c = strtok (0, " ");
-    }
-    if (cmd->hasCommand()) {
-            v.push_back(cmd);
-            
-            cmd = new Command();
-    }
-
-   /* for (unsigned i = 0; i < v.size(); ++i) {
-            v.at(i)->exec();
-
-            while (!(v.at(i)->succeeded) && (v.at(i)->lhs != 0)) {
-                if (v.size() == 1) {
-                    break;
-                }
-                else if (i == (v.size() - 2)) {
-                    v.pop_back();
-                    break;
+                cmd = new Command(); 
+            }
+            else if (c == ANDsym) {
+                if (!p->isEmpty()) {
+                    v.push_back(p);
+                    v.push_back(new And(p));
+                    p = new Precedence();
                 }
                 else {
-                    i += 2;
-                    v.at(i)->lhs = v.at(i - 2)->lhs;
-                    v.at(i)->exec();
+                    v.push_back(cmd); 
+                    v.push_back(new And(cmd));
+
+                    cmd = new Command();
                 }
             }
+            else if (c == ORsym) {
+                if (!p->isEmpty()) {
+                    v.push_back(p);
+                    v.push_back(new Or(p));
+                    p = new Precedence();
+                }
+                else {
+                    v.push_back(cmd); 
+                    v.push_back(new Or(cmd));
+
+                    cmd = new Command();
+                }
+            }
+            else {
+                cmd->addCmd(c); 
+            }
+
+            c = strtok (0, " ");
         }
-       */ 
-        return v.size();
+        
+        if (cmd->hasCommand()) {
+            v.push_back(cmd);
+            cmd = new Command();
+        }
+
+        
+        if (!p->isEmpty()) {
+            v.push_back(p);
+        }
+
+    
+
+    return v.size();
 }
 
-//test vector get correct number of command
 TEST(VectorTest, oneCmd) {
     string input = "echo A";
     int vSize = 0;
